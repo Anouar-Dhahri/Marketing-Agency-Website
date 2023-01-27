686f853b6933394defa6db7a4ce3ec538da992aa
Anouar Dhahri <anouar.dhahri1@gmail.com>
2023-01-27 23:06:23 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#252422",
+        secondary: "#1982c4",
+      },
+    },
+    screens: {
+      "2xl": { max: "1535px" },
+
+      xl: { max: "1279px" },
+
+      lg: { max: "1023px" },
+
+      md: { max: "767px" },
+
+      sm: { max: "639px" },
+    },
+  },
+  plugins: [],
+}
